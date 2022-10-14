59dd4a8b5427c973400b01177ee61fe31561a088
maaxg <maxgps5@hotmail.com>
2022-10-14 07:55:13 -0300

    home ts

---
+++ ts/home.ts
@@ -0,0 +1,43 @@
+const gymdaily = document.getElementById("gymdaily")
+const momentum = document.getElementById("momentum")
+const driveNeat = document.getElementById("driveNeat")
+const wiipo = document.getElementById("wiipo")
+const gaivota = document.getElementById("gaivota")
+const imuneKids = document.getElementById("imuneKids")
+
+const onClickOverlay = () => {
+
+}
+
+if(gymdaily) {
+  gymdaily.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+if(momentum) {
+  momentum.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+if(driveNeat) {
+  driveNeat.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+if(wiipo) {
+  wiipo.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+if(gaivota) {
+  gaivota.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+if(imuneKids) {
+  imuneKids.onclick = (ev) => {
+    onClickOverlay()
+  }
+}
+
+
